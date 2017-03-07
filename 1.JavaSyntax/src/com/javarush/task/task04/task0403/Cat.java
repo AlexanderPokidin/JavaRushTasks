460b444dcb3182767bd118829a71318090179aff
alexander <a.pokidin@gmail.com>
2017-03-07 11:52:40 +0200

    Commit JavaRush tasks

--- 1.JavaSyntax/src/com/javarush/task/task04/task0403/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0403/Cat.java
@@ -8,10 +8,13 @@ public class Cat {
     private String name;
 
     public void setName(String name) {
-        //напишите тут ваш код
+        this.name = name;
     }
 
     public static void main(String[] args) {
-
+        Cat cat = new Cat();
+        cat.setName("Bob");
     }
+
+
 }
