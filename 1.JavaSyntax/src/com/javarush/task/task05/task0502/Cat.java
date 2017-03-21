a893db58bc5b837d2c8cd4e5f3a859ceb2ec1788
alexander <a.pokidin@gmail.com>
2017-03-21 12:00:48 +0200

    Commit JavaRush tasks

--- 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
@@ -14,33 +14,34 @@ public class Cat {
     }
 
     public boolean fight(Cat anotherCat) {
+        return this.age + this.weight + this.strength > anotherCat.age + anotherCat.weight + anotherCat.strength;
 
 //        double x = this.strength * this.weight / this.age;
 //        double y = anotherCat.strength * anotherCat.weight / anotherCat.age;
 //        return x > y;
 
 
-        if ((this.weight * this.strength / this.age) > (anotherCat.weight * anotherCat.strength / anotherCat.age)){
-            return true;
-        } else{
-            return false;
-        }
+//        if ((this.weight * this.strength / this.age) > (anotherCat.weight * anotherCat.strength / anotherCat.age)){
+//            return true;
+//        } else{
+//            return false;
+//        }
     }
 
     public static void main(String[] args) {
-//        Cat cat1 = new Cat();
-//        cat1.name = "Boris";
-//        cat1.age = 5;
-//        cat1.weight = 4;
-//        cat1.strength = 9;
-//
-//        Cat cat2 = new Cat();
-//        cat2.name = "Vasily";
-//        cat2.age = 7;
-//        cat2.weight = 6;
-//        cat2.strength = 8;
-//
-//        System.out.println(cat1.fight(cat2));
-//        System.out.println(cat2.fight(cat1));
+        Cat cat1 = new Cat();
+        cat1.name = "Boris";
+        cat1.age = 5;
+        cat1.weight = 4;
+        cat1.strength = 9;
+
+        Cat cat2 = new Cat();
+        cat2.name = "Vasily";
+        cat2.age = 7;
+        cat2.weight = 6;
+        cat2.strength = 8;
+
+        System.out.println(cat1.fight(cat2));
+        System.out.println(cat2.fight(cat1));
     }
 }
