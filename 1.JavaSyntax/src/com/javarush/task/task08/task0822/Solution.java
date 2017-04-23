67a5a5aa0fc89597768a444f3ccfd2dbc93e4bb0
AlexanderPokidin <a.pokidin@gmail.com>
2017-04-23 18:37:20 +0300

    JavaRush task

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0822/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task08.task0822;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Минимальное из N чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Integer> integerList = getIntegerList();
+        System.out.println(getMinimum(integerList));
+    }
+
+    public static int getMinimum(List<Integer> array) {
+        int min = array.get(0);
+        for (int n: array) {
+            if (n < min){
+                min = n;
+            }
+        }
+        return min;
+    }
+
+    public static List<Integer> getIntegerList() throws IOException {
+        ArrayList<Integer> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < count; i++) {
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+        return list;
+    }
+}
