d2f60fa22a9d41e62221d30098d5bfadaad7b498
AlexanderPokidin <a.pokidin@gmail.com>
2017-04-23 21:18:42 +0300

    JavaRush task

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task08.task0823;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Омовение Рамы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        char[] strings = s.toCharArray();
+        strings[0] = Character.toUpperCase(strings[0]);
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i] == ' '){
+                strings[i + 1] = Character.toUpperCase(strings[i + 1]);
+            }
+        }
+
+        System.out.println(strings);
+    }
+}
